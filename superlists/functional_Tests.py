174f721187f08efda7247ebf53bcb5816eadcacd
jrottersman <rottersmanjake@gmail.com>
2015-05-27 18:09:04 -0400

    Front page HTML now generated from a template

--- superlists/functional_Tests.py
+++ superlists/functional_Tests.py
@@ -36,7 +36,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any(row.text == '1: foo' for row in rows)
+                any(row.text == '1: foo' for row in rows),
+                "New to-do item did not appear in table"
         )
         #there is still a textbox inviting her to enter items she enters another
         #item

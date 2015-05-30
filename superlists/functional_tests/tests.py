0988d0a90fa59a419858fbd8c7c23299ee8bcde3
jrottersman <rottersmanjake@gmail.com>
2015-05-30 16:19:49 -0400

    Functional test now exist for handling unique urls

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -35,9 +35,11 @@ class NewVisitorTest(LiveServerTestCase):
         #She types something like "foo"  into the textbox
         inputbox.send_keys('foo')
 
-        #When she hits enter the list is update with 1: item
+        #When she hits enter she is taken to a new url and the list is
+        #updated with 1: item
         inputbox.send_keys(Keys.ENTER)
-
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: foo')
         #there is still a textbox inviting her to enter items she enters another
         #item
@@ -48,7 +50,32 @@ class NewVisitorTest(LiveServerTestCase):
         #the page updates with her new item so the list is now "1: foo 2: bar"
         self.check_for_row_in_list_table('1: foo')
         self.check_for_row_in_list_table('2: bar')
-        #she reads that the site has generated a unique URL for her
-        self.fail('Finish the test!')
-        #she visits that url her to do list is still there
+        
+        #Another user called Francis comes along to our site
 
+        #We use a new browser session to make sure none of edith's 
+        #info comes through
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        #Francis vists the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('foo', page_text)
+        self.assertNotIn('bar', page_text)
+
+        #Francis starts a new list by entering a new item. He works in sales
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy Rolex')
+        inputbox.send_keys(Keys.ENTER)
+
+        #Francis gets his own unique url
+        francis_list_url = self.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        #There should be no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertIn('Buy milk', page_text)
+
+        #Satisfied, they go there seperate ways
